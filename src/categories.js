6ac5d32c84d5944b5d9bb38026a8f48f201e30e2
박태환 <xoghks8192@gmail.com>
2026-06-13 16:29:09 +0900

    Add insurance (보험) expense category

--- src/categories.js
+++ src/categories.js
@@ -1,6 +1,6 @@
 export const CATEGORIES = {
   income: ['월급', '용돈', '부수입', '기타수입'],
-  expense: ['식비', '교통', '주거/통신', '쇼핑', '의료', '문화/여가', '교육', '카드값', '기타지출'],
+  expense: ['식비', '교통', '주거/통신', '쇼핑', '의료', '보험', '문화/여가', '교육', '카드값', '기타지출'],
 }
 
 export const EXPENSE_CATEGORY_COLORS = {
@@ -9,6 +9,7 @@ export const EXPENSE_CATEGORY_COLORS = {
   '주거/통신': '#fdffb6',
   쇼핑: '#caffbf',
   의료: '#9bf6ff',
+  보험: '#c8b6ff',
   '문화/여가': '#a0c4ff',
   교육: '#bdb2ff',
   카드값: '#ffc6ff',
